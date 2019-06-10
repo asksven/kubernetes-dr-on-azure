f0479faea70e207a2bc0a84651ebf89908a6f897
Sven Knispel <sven.knispel@gmail.com>
2019-06-10 23:21:03 +0200

    We have to wait for the restore jobs to be finished before continuing

--- 11-restore-workloads.sh
+++ 11-restore-workloads.sh
@@ -13,3 +13,19 @@ do
   echo Most recent backup: $backup_name
   velero restore create --from-backup $backup_name
 done
+
+# wait for the namespaces to have been create
+for NS in $(cat namespaces)
+do
+  echo Checking for restore of namespace $NS
+  exists=0
+  while [ $exists -eq 0 ]; do
+    echo "Waiting for namespace..."
+    command=$(KUBECONFIG=./kubeconfig/config-$CLUSTER_NAME kubectl get ns $NS)
+    if [ $? -eq 0 ]; then
+      exists=1
+      echo Namespace $NS exists
+    fi
+    [ $exists -eq 0  ] && sleep 10
+  done
+done
